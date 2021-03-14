54d71863c866ee74b081ce01df9104f5a6c7ba03
tikhono <tihonovandrey11@gmail.com>
2021-03-15 00:25:46 +0200

    rearrange project from binaries to libs

---
+++ hacks/general/encoding/bytes_and_big_integers/src/lib.rs
@@ -0,0 +1,17 @@
+#[cfg(test)]
+mod tests {
+    use uint::construct_uint;
+
+    construct_uint! {pub struct U1024(16);}
+    #[test]
+    fn capture_the_flag() {
+        let string =
+            "11515195063862318899931685488813747395775516287289682636499965282714637259206269";
+        let num = U1024::from_dec_str(string).unwrap();
+        let bytes: &mut [u8] = &mut [0; 16 * 8];
+        num.to_big_endian(bytes);
+        for i in 0..bytes.len() {
+            print!("{}", bytes[i] as char);
+        }
+    }
+}
---
+++ hacks/general/encoding/hex/src/lib.rs
@@ -0,0 +1,16 @@
+#[cfg(test)]
+mod tests {
+    use hex::FromHex;
+
+    #[test]
+    fn capture_the_flag() {
+        match Vec::from_hex("63727970746f7b596f755f77696c6c5f62655f776f726b696e675f776974685f6865785f737472696e67735f615f6c6f747d") {
+            Ok(vec) => {
+                for b in vec {
+                    print!("{}", b as char);
+                }
+            }
+            Err(_e) => {}
+        }
+    }
+}
