545b06a40b1d49e25c855f5727968b28d536624f
tikhono <tikhonov.andrii.m@gmail.com>
2021-03-25 14:12:59 +0200

    add round keys and structure of aes

--- hacks/block_ciphers/aes/structure_of_aes/README.md
+++ hacks/block_ciphers/aes/structure_of_aes/README.md
@@ -31,5 +31,3 @@
 > Included is a `bytes2matrix` function for converting our initial plaintext block into a state matrix. Write a `matrix2bytes` function to turn that matrix back into bytes, and submit the resulting plaintext as the flag.
 >
 > [matrix.py](https://cryptohack.org/static/challenges/matrix_e1b463dddbee6d17959618cf370ff1a5.py)
-> 
-> 
--- hacks/block_ciphers/aes/structure_of_aes/src/lib.rs
+++ hacks/block_ciphers/aes/structure_of_aes/src/lib.rs
@@ -1,5 +1,12 @@
+use nalgebra::Matrix4;
+
+pub fn matrix2bytes(matrix: Matrix4<u8>) -> Vec<u8> {
+    matrix.transpose().as_slice().to_vec()
+}
+
 #[cfg(test)]
 mod tests {
+    use crate::matrix2bytes;
     use ascii::IntoAsciiString;
     use nalgebra::Matrix4;
 
@@ -8,7 +15,7 @@ mod tests {
         let matrix = Matrix4::new(
             99, 114, 121, 112, 116, 111, 123, 105, 110, 109, 97, 116, 114, 105, 120, 125,
         );
-        let vec = matrix.transpose().as_slice().to_vec();
+        let vec = matrix2bytes(matrix);
         println!("{}", vec.into_ascii_string().unwrap());
     }
 }
