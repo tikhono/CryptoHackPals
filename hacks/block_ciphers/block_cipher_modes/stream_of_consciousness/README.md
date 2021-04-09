b01312bc3b0605e54e7f949b7f66145743e79162
tikhono <tikhonov.andrii.m@gmail.com>
2021-04-09 11:30:28 +0300

    flipping cookie and unfinished stream of contsciousness for discussion

---
+++ hacks/block_ciphers/block_cipher_modes/stream_of_consciousness/README.md
@@ -0,0 +1,14 @@
+[CryptoHack – Block Ciphers challenges](https://cryptohack.org/challenges/aes/)
+
+> Talk to me and hear a sentence from my encrypted stream of consciousness.
+>
+> Play at [http://aes.cryptohack.org/stream\_consciousness](http://aes.cryptohack.org/stream_consciousness)
+
+> ### How to:
+> Run all tests from this package:
+>
+>     cargo test --package stream_of_consciousness --lib tests
+>
+> Capture the flag:
+>
+>     cargo test --package stream_of_consciousness --lib tests::capture_the_flag -- --exact
---
+++ hacks/block_ciphers/block_cipher_modes/stream_of_consciousness/src/lib.rs
@@ -0,0 +1,24 @@
+mod tests {
+    use ascii::IntoAsciiString;
+    use ecb_cbc_wtf::get_response;
+    use std::collections::BTreeMap;
+
+    #[test]
+    fn capture_the_flag() {
+        let mut map = BTreeMap::new();
+
+        for _i in 0..200 {
+            let addr = "http://aes.cryptohack.org/stream_consciousness/encrypt/".to_string();
+            let ciphertext = get_response(addr);
+
+            map.insert(ciphertext.len() / 2, ciphertext);
+        }
+        for ciph in map {
+            print!("{:?}\t", ciph.0);
+            for chunk in ciph.1.as_bytes().chunks(32) {
+                print!("{:?}", chunk.into_ascii_string().unwrap());
+            }
+            println!();
+        }
+    }
+}
