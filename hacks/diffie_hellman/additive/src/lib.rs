7d560f0236bc2711319902d82622227e52a78219
tikhono <tikhonov.andrii.m@gmail.com>
2021-05-23 17:11:07 +0300

    refactor

--- hacks/diffie_hellman/additive/src/lib.rs
+++ hacks/diffie_hellman/additive/src/lib.rs
@@ -1,10 +1,13 @@
 #[cfg(test)]
 mod tests {
+    use diffie_hellman_starter_1::mod_inverse;
     use num::bigint::BigInt;
     use openssl::symm::{decrypt, Cipher};
     use serde::Deserialize;
     use sha1::{Digest, Sha1};
-    use telnet::{Telnet, TelnetEvent};
+    use tokio::io::AsyncBufReadExt;
+    use tokio::io::BufReader;
+    use tokio::net::TcpStream;
 
     const BLOCK_SIZE: usize = 16;
 
@@ -26,71 +29,55 @@ mod tests {
         encrypted: String,
     }
 
-    #[test]
-    fn capture_the_flag() {
-        let mut connection = Telnet::connect(("134.122.111.232", 13380), 4096)
-            .expect("Couldn't connect to the server...");
-        println!("Connected to server");
+    #[tokio::test]
+    async fn capture_the_flag() {
+        let stream = TcpStream::connect("134.122.111.232:13380").await.unwrap();
+        let mut stream = BufReader::new(stream);
 
-        let event = connection.read().expect("Read Error");
-        match event {
-            TelnetEvent::Data(_) => {} //Need 2 reads, in this buffer just "Intercepted from Alice"
-            _ => println!("Error"),
-        }
+        let mut line = String::new();
+        stream.read_line(&mut line).await.unwrap();
+        let alice_data: FromAlice =
+            serde_json::from_str(&line.strip_prefix("Intercepted from Alice: ").unwrap()).unwrap();
 
-        let event = connection.read().expect("Read Error");
-        match event {
-            TelnetEvent::Data(buffer) => {
-                let json_strings: Vec<&[u8]> = buffer.split(|n| *n == b'\n').collect();
+        let mut line = String::new();
+        stream.read_line(&mut line).await.unwrap();
+        let bob_data: FromBob =
+            serde_json::from_str(&line.strip_prefix("Intercepted from Bob: ").unwrap()).unwrap();
 
-                let alice_data: FromAlice = serde_json::from_slice(json_strings[0]).unwrap();
-                let bob_data: FromBob = serde_json::from_slice(&json_strings[1][22..]).unwrap(); //Cut "Intercepted from Bob"
-                let flag: Flag = serde_json::from_slice(&json_strings[2][24..]).unwrap(); //Cut "Intercepted from Alice"
+        let mut line = String::new();
+        stream.read_line(&mut line).await.unwrap();
+        let flag: Flag =
+            serde_json::from_str(&line.strip_prefix("Intercepted from Alice: ").unwrap()).unwrap();
 
-                println!("{:#?}", alice_data);
-                println!("{:#?}", bob_data);
-                println!("{:#?}", flag);
+        let iv = hex::decode(flag.iv).unwrap();
+        let flag = hex::decode(flag.encrypted).unwrap();
 
-                let iv: Vec<u8> = hex::decode(flag.iv).unwrap();
+        //First 2 bytes are "0x", so cut off them to successfully parse bigint.
+        let alice_pub = BigInt::parse_bytes(&alice_data.A.as_bytes()[2..], 16).unwrap();
+        let bob_pub = BigInt::parse_bytes(&bob_data.B.as_bytes()[2..], 16).unwrap();
+        let p = BigInt::parse_bytes(&alice_data.p.as_bytes()[2..], 16).unwrap();
+        let g = BigInt::parse_bytes(&alice_data.g.as_bytes()[2..], 16).unwrap();
 
-                let flag = hex::decode(flag.encrypted).unwrap();
+        let mut alice_secret = BigInt::parse_bytes(b"0", 10).unwrap();
 
-                //First 2 bytes are "0x", so cut off them to successfully parse bigint.
-                let alice_pub = BigInt::parse_bytes(&alice_data.A.as_bytes()[2..], 16).unwrap();
-                let bob_pub = BigInt::parse_bytes(&bob_data.B.as_bytes()[2..], 16).unwrap();
-                let p = BigInt::parse_bytes(&alice_data.p.as_bytes()[2..], 16).unwrap();
-                let g = BigInt::parse_bytes(&alice_data.g.as_bytes()[2..], 16).unwrap();
+        let inv = mod_inverse(g, p.clone()).unwrap();
+        alice_secret = alice_pub * inv % &p;
 
-                let mut alice_secret = BigInt::parse_bytes(b"0", 10).unwrap();
+        let shared_secret = bob_pub * &alice_secret % &p;
 
-                for n in 0..u64::MAX {
-                    println!("{}", n);
-                    if (&bob_pub * ((&alice_pub + (&p * n)) / &g)) % &p
-                        == (&alice_pub * ((&bob_pub + (&p * n)) / &g)) % &p
-                    {
-                        alice_secret = (&alice_pub + (&p * n)) / &g;
-                        break;
-                    }
-                }
+        let mut hasher = Sha1::new();
+        hasher.update(&shared_secret.to_str_radix(10));
+        let key = hasher.finalize().to_vec();
+        let key = key.get(0..BLOCK_SIZE).unwrap();
 
-                let shared_secret = bob_pub * &alice_secret % &p;
-
-                let mut hasher = Sha1::new();
-                hasher.update(&shared_secret.to_str_radix(10));
-                let key = hasher.finalize().to_vec();
-                let key = key.get(0..BLOCK_SIZE).unwrap();
-
-                let cipher = Cipher::aes_128_cbc();
-                match decrypt(cipher, key, Some(&iv), &flag) {
-                    Ok(text) => {
-                        println!("{}", String::from_utf8_lossy(&text))
-                    }
-                    Err(_) => {
-                        println!("Unable to decrypt");
-                    }
-                };
+        let cipher = Cipher::aes_128_cbc();
+        match decrypt(cipher, key, Some(&iv), &flag) {
+            Ok(text) => {
+                println!("{}", String::from_utf8_lossy(&text))
             }
-            _ => println!("Error"),
-        }
+            Err(_) => {
+                println!("Unable to decrypt");
+            }
+        };
     }
 }
